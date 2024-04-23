526137bcdde9fd9366b39144714c3ebbfc12a98e
Toni Schmidbauer <toni@stderr.at>
2024-04-23 14:42:12 +0200

    import java wellgrounded examples

---
+++ Java/WellGrounded/src/at/stderr/ScratchImpl.java
@@ -0,0 +1,18 @@
+package at.stderr;
+
+public class ScratchImpl {
+    private static ScratchImpl inst = null;
+
+    private ScratchImpl() {
+
+    }
+
+    private void run() {
+
+    }
+
+    public static void main(String[] args) {
+        inst = new ScratchImpl();
+        inst.run();
+    }
+}
