b2bbcb1ef06f982ef9565cb44a83fca2889347b1
Toni Schmidbauer <toni@stderr.at>
2024-07-27 16:12:03 +0200

    all work and not play

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,8 @@
-((nil
+(("openshift/"
   . ((eglot-workspace-configuration
       . (:yaml (:schemas (:kubernetes "*.yaml"
-		          :https://raw.githubusercontent.com/melmorabity/openshift-json-schemas/main/v4.14-local/_definitions.json "*.yaml")))))))
+				      :https://raw.githubusercontent.com/melmorabity/openshift-json-schemas/main/v4.14-local/_definitions.json "*.yaml"))))))
+ ("ansible/"
+  . ((eglot-workspace-configuration
+      :ansible (:validation
+		(:enabled t :lint (:enabled t)))))))
